aa30246c7d69970a95a39919fd551b67f65e6c70
alexandru2908 <predescualex81@gmail.com>
2026-01-18 10:54:30 +0200

    nimic

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,17 @@
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 
-
+if (typeof window !== 'undefined') {
+  const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+  const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST;
+  
+  if (posthogKey && posthogHost) {
+    posthog.init(posthogKey, {
+      api_host: posthogHost,
+      person_profiles: 'identified_only',
+    })
+  }
+}
 
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
     return <PostHogProvider client={posthog}>{children}</PostHogProvider>
